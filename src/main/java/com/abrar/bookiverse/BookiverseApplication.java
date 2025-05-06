17eb117771ce1ab841765924c32369f83c5f4fc2
AlchemistReturns <abrarhasan2003@gmail.com>
2025-05-06 16:08:10 +0600

    Initial project setup

---
+++ src/main/java/com/abrar/bookiverse/BookiverseApplication.java
@@ -0,0 +1,13 @@
+package com.abrar.bookiverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookiverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookiverseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abrar/bookiverse/BookiverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrar.bookiverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookiverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
